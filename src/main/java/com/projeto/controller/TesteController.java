36b607c7514ee16984782b208f68c2a7fed31182
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2020-07-07 21:29:39 -0300

    Implemetação do CDI

--- src/main/java/com/projeto/controller/TesteController.java
+++ src/main/java/com/projeto/controller/TesteController.java
@@ -1,10 +1,13 @@
 package com.projeto.controller;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
+@RequestScoped
 public class TesteController {
-    public String getTeste(){
+
+    public String teste(){
         System.out.println("Controller funcionando");
         return "Tudo certo";
     }
